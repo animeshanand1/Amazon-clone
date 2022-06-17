69f72e2da9b7d2adc5ae92d30b7651f5f8cc13cd
animeshanand1 <animeshanand9@gmail.com>
2022-06-17 21:45:02 +0530

    second commit

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,22 +1,25 @@
-
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import Product from './Product';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { Routes ,Route } from 'react-router-dom';
+import Home from "./Home";
+import Checkout from "./Checkout";
+import Header from "./Header";
 
 function App() {
   return (
-    // BEM
+    <Router>
     <div className="app">
       
-      <Header/>
-      <Home/>
-      
-    
-      
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+
+        </Routes>
       
     </div>
+    </Router>
   );
 }
-
 export default App;
