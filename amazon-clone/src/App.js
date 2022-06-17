d132c4719308d0e14533d5f4f2f7ddef46228f20
animeshanand1 <animeshanand9@gmail.com>
2022-06-17 20:00:28 +0530

    first commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Product from './Product';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      
+      <Header/>
+      <Home/>
+      
+    
+      
+      
+    </div>
+  );
+}
+
+export default App;
