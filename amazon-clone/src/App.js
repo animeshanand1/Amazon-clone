640bd4a7526813a31929dd1e2972410922a1ef00
animeshanand1 <animeshanand9@gmail.com>
2022-06-21 13:41:12 +0530

    added login page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,16 +5,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Header from "./Header";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
     <div className="app">
+       
       
         <Header/>
         <Routes>
+        
           <Route path="/" element={<Home />} />
+          
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
 
         </Routes>
       
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -16,8 +16,10 @@ function Header() {
     </div>
     <div className='header_nav'>
         <div className='header_option'>
+            <Link to='/login'>
             <span className='header_optionLineOne'>Hello Guest</span>
             <span className='header_optionLineTwo'>Sign In</span>
+            </Link>
         </div>
         <div className='header_option'>
             <span className='header_optionLineOne'>Requests</span>
