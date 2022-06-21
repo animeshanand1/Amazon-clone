1b1ef8b4f8824b2f7ce315a68c0f9334e0a61d69
animeshanand1 <animeshanand9@gmail.com>
2022-06-21 13:57:27 +0530

    added login page

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTwPCKMJHTjtCo4WC_QfNCKn8Y4uyBcY0",
+    authDomain: "clone-770c3.firebaseapp.com",
+    projectId: "clone-770c3",
+    storageBucket: "clone-770c3.appspot.com",
+    messagingSenderId: "811684008496",
+    appId: "1:811684008496:web:8311543a355a18947393f5",
+    measurementId: "G-DBQWW9C3LL"
+  };
